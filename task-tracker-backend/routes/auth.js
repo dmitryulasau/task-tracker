a52644455ec50721b67b51608ed02e10ab66c748
DMITRY ULASAU <dmitrushok@gmail.com>
2023-04-18 21:23:32 -0400

    feat: add login route

--- task-tracker-backend/routes/auth.js
+++ task-tracker-backend/routes/auth.js
@@ -23,5 +23,20 @@ router.post("/register", async (req, res) => {
 });
 
 // LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong username!");
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(400).json("Wrong password!");
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
